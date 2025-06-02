c7dcd78061cb0880ba683de84cf5ad8e8dbb9ff7
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-06-02 14:52:11 +0200

    Add fixes bookRepository classe

---
+++ src/main/java/org/library/Main.java
@@ -0,0 +1,21 @@
+package org.library;
+
+import org.library.config.DBManager;
+import org.library.controller.BookController;
+import org.library.repository.BookRepository;
+import org.library.view.BookView;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection connection = DBManager.initConnection();
+
+        BookRepository repository = new BookRepository(connection);
+        BookController controller = new BookController(repository);
+        BookView view = new BookView(controller);
+        controller.setView(view);
+
+        view.start();
+    }
+}
--- src/main/java/org/library/config/DBManager.java
+++ src/main/java/org/library/config/DBManager.java
@@ -6,28 +6,38 @@ import java.sql.SQLException;
 import java.sql.SQLOutput;
 
 public class DBManager {
+
     private static final String URL = "jdbc:mysql://localhost:3306/library";
     private static final String USER = "root";
-    private static final String PASSWORD = String.join ("PASSWORD");
+    private static final String PASSWORD = ("root");
 
     private static Connection connection;
 
-    public static Connection initConnection() {
+    public static Connection initConnection() throws RuntimeException {
         try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Connected to MySQL database");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Connected to MySQL database");
+            }
         } catch (SQLException exception) {
-            System.out.println(exception.getMessage());
+            System.out.println("SQL error: " + exception.getMessage());
+            throw new RuntimeException("Failed to connect to DB");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found.");
+            throw new RuntimeException(e);
         }
         return connection;
     }
     public static void closeConnection() {
         try {
-            connection.close();
-            System.out.println("Connection closed successfully.");
-        } catch (SQLException exception) {
-            System.out.println(exception.getMessage());
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Connection closed.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error closing connection: " + e.getMessage());
         }
     }
 }
-
